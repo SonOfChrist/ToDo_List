46ed0e2e244e7b161bee34a6b8764e54dee6fbfa
SoftwareDev Stan <mochogestanley80@gmail.com>
2024-09-19 17:20:30 +0300

    Todo-list-Dejan-Stan-Project

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # ToDo_List
+## all  Javascript programme
+1. Save the Data
+2. Generate the HTML [through Html Elements]
+3. Make it interactive
+- Html Css Javascript {Array & Loops [Looping Through the arrays]}
+
+## Snapshot Of The List
+ <img src="Images/todoList.png" alt="to-Do-Stanley-mochoge-List">
--- main.js
+++ main.js
@@ -1,17 +1,38 @@
-const todoList = ['make dinner', 'wash dishes'];
+const todoList = [{
+   name: 'make dinner',
+   dueDate: '2024-8-19'
+}, {
+   name:'wash dishes',
+   dueDate: '2024-8-19'}];
 
-let todoListHTMl = '';
+renderToDoList();
 
-for(let i = 0; i < todoList.length; i++){
-   const todo = todoList[i];
-   const html = `<p>${todo}</p>`;
-   todoListHTMl += html;
+function renderToDoList(){
+   let todoListHTMl = '';
+
+      for(let i = 0; i < todoList.length; i++){
+         const todoObject = todoList[i];
+         //const {name} = todoObject;
+         const {name, dueDate} = todoObject;
+         const html = `
+            <div>${name}</div>
+            <div>${dueDate}</div>
+            <button onclick="
+               todoList.splice(${i}, 1)
+               renderToDoList();
+            " class="delete-todo-button">Delete</button> 
+               `;
+         todoListHTMl += html;
+      }
+
+      document.querySelector('.js-todo-list').innerHTML = todoListHTMl;
 }
-
 function addTodo() {
    const inputElement = document.querySelector('.js-name-input');
    const name = inputElement.value;
-   todoList.push(name);
-   console.log(todoList)
+   const dateInputElement = document.querySelector('.js-due-date-input');
+   const dueDate = dateInputElement.value;
+   todoList.push({name, dueDate});
    inputElement.value = '';
+   renderToDoList();
 }
